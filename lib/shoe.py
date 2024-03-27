7a360f825f89b9849b799c2ebe96220cf5f11aae
illyalane <lane_illya@yahoo.com>
2024-03-27 19:57:56 -0400

    completed lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.repaired = False
+        self.condition = 'Used'
+
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self,value):
+        if type(value) == int:
+            print("The shoe has been repaired.")   
+            self._size = value
+        else:
+            print("size must be an integer")  
+          
+    def cobble(self):
+        self.repaired = True
+        self.condition = 'New'
+        print("Your shoe is as good as new!")      
+        
